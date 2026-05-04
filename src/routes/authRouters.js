92ed6b9b8130653449d346736a05206f7c9bcbae
267T <vanloa402@gmail.com>
2026-05-04 14:11:25 +0700

    fix 2

--- src/routes/authRouters.js
+++ src/routes/authRouters.js
@@ -1,15 +1,20 @@
-/* file này có chức năng định nghĩa cho các endpoint và nối chúng với controller */
-
 const express = require('express');
 const router = express.Router();
-const { register, login, forgotPassword, resetPassword,logout  } = require('../controllers/authController');
 
-const router = express.Router();
+const {
+    register,
+    login,
+    forgotPassword,
+    resetPassword,
+    logout
+} = require('../controllers/authController');
+
+const authMiddleware = require('../middlewares/authMiddleware');
 
 router.post('/register', register);
 router.post('/login', login);
-router.post('/logout', authMiddleware, logout);
 router.post('/forgot-password', forgotPassword);
 router.post('/reset-password/:token', resetPassword);
+router.post('/logout', authMiddleware, logout);
 
 module.exports = router;
