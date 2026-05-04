58eaec62bb64d5e350d5ee8ce1f7fae74563e9d2
267T <vanloa402@gmail.com>
2026-05-04 11:40:38 +0700

    fix

--- src/app.js
+++ src/app.js
@@ -16,10 +16,7 @@ const orderRoutes = require('./routes/orderRoutes');
 
 const app = express();
 
-app.use(cors({
-  origin: process.env.CLIENT_URL,
-  credentials: true
-}));
+app.use(cors());
 
 app.use(express.json());
 
