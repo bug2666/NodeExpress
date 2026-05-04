876ee73bea36020e9610c40c2cc91f6fe4868fc2
267T <vanloa402@gmail.com>
2026-05-04 10:30:41 +0700

    add check

--- src/server.js
+++ src/server.js
@@ -1,3 +1,11 @@
+process.on('uncaughtException', (error) => {
+  console.error('Uncaught Exception:', error);
+});
+
+process.on('unhandledRejection', (error) => {
+  console.error('Unhandled Rejection:', error);
+});
+
 require('dotenv').config();
 const app = require('./app');
 const pool = require('./configs/mysql');
