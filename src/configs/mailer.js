c18f6c1aef9b10aa8791337fe7c4b81805bff7a3
267T <vanloa402@gmail.com>
2026-05-04 14:27:24 +0700

    fix email v1

--- src/configs/mailer.js
+++ src/configs/mailer.js
@@ -1,12 +1,16 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    family: 4,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     }
 });
+
 
 const sendMail = async ({ to, subject, html }) => {
     await transporter.sendMail({
